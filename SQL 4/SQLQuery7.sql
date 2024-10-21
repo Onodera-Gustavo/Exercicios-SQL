a2d464dfc21b27aa902d2ab6ef26212f37ac91f2
Gustavoo <165702804+Onodera-Gustavo@users.noreply.github.com>
2024-10-21 14:48:07 -0300

    Delete SQL 4 directory again

--- SQL 4/SQLQuery7.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	DimProduct.ProductName,
-	DimProduct.ProductKey,
-	DimProductSubcategory.ProductSubcategoryName
-FROM DimProduct 
-INNER JOIN DimProductSubcategory
-ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductSubcategoryKey
-WHERE DimProduct.ProductName IS NULL
